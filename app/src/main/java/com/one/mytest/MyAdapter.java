252448647db9fc88af51fa7db79c5addaac8bb47
HarshSinghRajawat <harshsinghrajawat86@gmail.com>
2021-05-18 02:03:22 +0530

    Final

--- app/src/main/java/com/one/mytest/MyAdapter.java
+++ app/src/main/java/com/one/mytest/MyAdapter.java
@@ -1,14 +1,12 @@
 package com.one.mytest;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,6 +22,7 @@ public class MyAdapter extends ArrayAdapter<Data> {
         Log.i("adapter","Adapter Created");
     }
 
+    @SuppressLint("SetTextI18n")
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -33,20 +32,28 @@ public class MyAdapter extends ArrayAdapter<Data> {
             listItem= LayoutInflater.from(getContext()).inflate(R.layout.adapter_layout,parent,false);
         }
         Data curWord=getItem(position);
+
         TextView userId=listItem.findViewById(R.id.userID);
         TextView ID=listItem.findViewById(R.id.ID);
         TextView title=listItem.findViewById(R.id.des);
         ConstraintLayout background=listItem.findViewById(R.id.background);
-        if(curWord.is_done){
-            background.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.if_true));
-        }else{
-            background.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.if_false));
-        }
+        setColor(curWord,background);
+        background.setOnClickListener(view -> {
+            curWord.is_done=!curWord.is_done;
+            setColor(curWord,background);
+        });
 
         userId.setText(curWord.getUid()+"");
         ID.setText(curWord.getId()+"");
         title.setText(curWord.getTitle());
 
         return listItem;
+    }
+    private void setColor(Data cur,ConstraintLayout background){
+        if(cur.is_done){
+            background.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.if_true));
+        }else{
+            background.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.if_false));
+        }
     }
 }
